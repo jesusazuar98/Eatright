bba67f1d3535154bb333bd0830bd82a302866f15
domingo <domocu4299@gmail.com>
2023-05-14 10:40:42 +0200

    index

--- index.php
+++ index.php
@@ -11,35 +11,51 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Abel&display=swap" rel="stylesheet" />
+    <link href="styles/index.css" rel="stylesheet" />
     <title>Eatright</title>
 </head>
 
 <body>
+<div class="wrapper">
+    <header>
+        <div class="logo">
+            <h1>EatRight</h1>
+        </div>
+        <nav>
+          <ul>
+            <li><a href="alimentos.php">Alimentos</a></li>
+            <?php
+            include('./user.php');
 
-    <a href="alimentos.php">Alimentos</a>
-    <?php
-    include('./user.php');
+            if (isset($_SESSION['usuario'])) {
 
-    if (isset($_SESSION['usuario'])) {
+                $user = unserialize($_SESSION['usuario']);
 
-        $user = unserialize($_SESSION['usuario']);
+                $data = $user->getUser();
+                echo "<p>" . $data['id'] . " " . $data['username'] . "</p>";
 
-        $data = $user->getUser();
-        echo "<p>" . $data['id'] . " " . $data['username'] . "</p>";
+                ?>
+                <li><a href="logout.php">Cerrar sesión</a></li>
 
-        ?>
-        <a href="logout.php">Cerrar sesión</a>
-
-        <?php
-    } else {
+                <?php
+            } else {
 
 
-        ?>
-        <a href="login.php">Iniciar sesión</a>
+                ?>
+                <a href="login.php">Iniciar sesión</a>
 
-        <?php
-    }
-    ?>
+                <?php
+            }
+            ?>
+            </nav>
+    </header>
+</div>
+<footer class="footer">
+    <p>Eatright&#169</p>
+    <p>Todos los derechos reservados a Jesus y Domingo</p>
+    <a href="#">Sobre Nosotros</a>
+</footer>
 </body>
 
 </html>
