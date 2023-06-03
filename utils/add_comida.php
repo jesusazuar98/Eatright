f3107ec8f07e3ee7762df7c31d4474c8aa34847b
Jesus Adrian Sanchez Azuar <jesusazuar@gmail.com>
2023-06-03 10:18:49 +0200

    Añadir favorito a comida

--- utils/add_comida.php
+++ utils/add_comida.php
@@ -1,14 +1,27 @@
 <?php
 include_once "../classes/alimentos.php";
+include_once "../classes/user.php";
 session_start();
 
 if (!isset($_SESSION['usuario'])) {
-    return false;
+    echo "<script>alert('Debe iniciar sesion.'); window.location.href='../index.php'</script>";
 }
 
 $alimentos = new Alimentos();
+$user = unserialize($_SESSION['usuario']);
+$id_u = $user->getUser()['id'];
 
-$result = $alimentos->buscar_alimentos($_POST['n_alimento'], $_POST['a_marca']);
+
+if ($_POST['check_fav'] == 'f') {
+    $result = $alimentos->buscar_alimentos($_POST['n_alimento'], $_POST['a_marca']);
+
+} else {
+
+    $result = $alimentos->buscar_favoritos($_POST['n_alimento'], $_POST['a_marca'], $id_u, true);
+
+}
+
+
 
 
 
@@ -17,14 +30,21 @@ if ($result == 0) {
     $code = "<div id='c1'>No se ha encontrado ningun alimento</div>";
 } else {
     $code = "<div id='c1'><ul>";
-    for ($i = 0; $i < sizeof($result); $i++) {
 
-        $r = json_encode($result[$i]);
+    if ($_POST['check_fav'] == 'f') {
+        for ($i = 0; $i < sizeof($result); $i++) {
 
-        $text = $result[$i]['marca'] . ", " . $result[$i]['porcion'] . " (gr o ml), " . $result[$i]['kcal'] . "kcal";
+            $r = json_encode($result[$i]);
 
-        $code .= "<li onclick='muestraAlimentos($r)'><a href='#c1'>" . $result[$i]['nombre'] . "</a><p>" . $text . "</p></li>";
+            $text = $result[$i]['marca'] . ", " . $result[$i]['porcion'] . " (gr o ml), " . $result[$i]['kcal'] . "kcal";
+
+            $code .= "<li onclick='muestraAlimentos($r)'><a href='#c1'>" . $result[$i]['nombre'] . "</a><p>" . $text . "</p></li>";
+        }
+    } else {
+
+        $code .= $result;
     }
+
 
     $code .= "</ul>";
 
